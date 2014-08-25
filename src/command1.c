26c7a7053134cc8f3125a99fe985ccb7a061caec
maciel310 <maciel310@gmail.com>
2014-08-24 22:41:09 -0700

    Added proper command line argument parsing.

--- src/command1.c
+++ src/command1.c
@@ -2,15 +2,18 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <popt.h>
 #include "firebase.h"
 #include "serial.h"
 #include "gpio.h"
 
 
-bool useSerial = true;
+enum sender_type { SERIAL, GPIO };
+enum sender_type selected_sender = SERIAL;
+
 
 void init_sender() {
-  if (useSerial) {
+  if (selected_sender == SERIAL) {
     init_sender_serial();
   } else {
     init_sender_gpio();
@@ -18,7 +21,7 @@ void init_sender() {
 }
 
 void send_command(bool isOn, int device) {
-  if (useSerial) {
+  if (selected_sender == SERIAL) {
     send_command_serial(isOn, device);
   } else {
     send_command_gpio(isOn, device);
@@ -32,16 +35,49 @@ void parse_event_string(char* event) {
 }
 
 int main(int argc, char *argv[]) {
+  int c;
+  int device = 0;
+  bool isOn = false;
+  char firebase_url[255];
+  char *firebase_host = "";
+
+  poptContext pc;
+  struct poptOption po[] = {
+    {"use-serial", 's', POPT_ARG_VAL, &selected_sender, SERIAL, "Use Serial sender", ""},
+    {"use-gpio", 'g', POPT_ARG_VAL, &selected_sender, GPIO, "Use GPIO sender", ""},
+    {"device", 'd', POPT_ARG_INT, &device, 0, "Device to command", ""},
+    {"on", '1', POPT_ARG_VAL, &isOn, true, "Turns selected device on", ""},
+    {"off", '0', POPT_ARG_VAL, &isOn, false, "Turns selected device off", ""},
+    {"firebase", 'f', POPT_ARG_STRING, &firebase_host, 0, "Firebase hostname to connect to.", ""},
+    POPT_AUTOHELP
+    {NULL}
+  };
+
+  pc = poptGetContext(NULL, argc, (const char **)argv, po, 0);
+
+  if (argc < 2) {
+    poptPrintUsage(pc, stderr, 0);
+    return 1;
+  }
+
+  int val;
+  while ((val = poptGetNextOpt(pc)) >= 0) {
+  }
+
+  if (val != -1) {
+    printf("ERROR: %d\n\n", val);
+  }
+
+
   init_sender();
 
-  if(argc > 1) {
+  if (firebase_host[0] == 0) {
     printf("Commanding remote\n\n");
   
-    bool isOn = strcmp(argv[1], "on") == 0 ? true : false;
-  
-    send_command(isOn, argv[2][0] - '0');
+    send_command(isOn, device);
   } else {
-    firebase_set_url("https://test.firebaseio.com/me.json");
+    sprintf(firebase_url, "https://%s.firebaseio.com/me.json", firebase_host);
+    firebase_set_url(firebase_url);
     firebase_set_callback("/event", parse_event_string);
 
     sleep(10);
